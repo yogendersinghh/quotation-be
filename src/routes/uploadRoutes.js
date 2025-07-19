e54e4b84cfd7fcaf652725a4aa79792540049fd4
Yogender Singh <yogender.s@opensenselabs.com>
2025-07-19 16:19:36 +0530

    minor bug fixes

--- src/routes/uploadRoutes.js
+++ src/routes/uploadRoutes.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const { uploadImage, uploadSignature, handleUploadError, uploadDataSheetOrCatalog } = require('../controllers/uploadController');
+const { uploadImage, uploadSignature, handleUploadError, uploadDataSheetOrCatalog,upload } = require('../controllers/uploadController');
 const { auth, isAdmin } = require('../middleware/auth');
 const uploadSignatureMiddleware = require('../middleware/signatureUpload');
 const multer = require('multer');
 const path = require('path');
+const fs = require("fs")
 
 // Dynamic storage destination based on 'type' in the request body
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    const type = req.body.type;
-    let folder = 'uploads';
+    const type = req.query.type;
+    let folder;
     if (type === 'datasheet') folder = 'public/datasheet';
     else if (type === 'catalog') folder = 'public/catalog';
+    if (folder) {
+      if (!fs.existsSync(folder)) {
+        fs.mkdirSync(folder, { recursive: true });
+      }
+    }
     cb(null, folder);
   },
   filename: function (req, file, cb) {
@@ -21,7 +27,7 @@ const storage = multer.diskStorage({
   }
 });
 
-const upload = multer({ storage });
+const sheetUpload = multer({ storage });
 
 // Upload product image (admin only)
 router.post('/product-image', auth, isAdmin, upload.single('image'), uploadImage);
@@ -30,7 +36,7 @@ router.post('/product-image', auth, isAdmin, upload.single('image'), uploadImage
 router.post('/signature', auth, uploadSignatureMiddleware.single('signature'), uploadSignature);
 
 // Route for uploading data sheet or catalog
-router.post('/datasheet-or-catalog', upload.single('file'), handleUploadError, uploadDataSheetOrCatalog);
+router.post('/datasheet-or-catalog', sheetUpload.single('file'), handleUploadError, uploadDataSheetOrCatalog);
 
 // Error handling middleware
 router.use(handleUploadError);
