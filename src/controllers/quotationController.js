512cd91fafbc0abf6bd686c869ab33531db6b4c1
Yogender Singh <yogender.s@opensenselabs.com>
2025-10-11 16:50:39 +0530

    fix minor issue

--- src/controllers/quotationController.js
+++ src/controllers/quotationController.js
@@ -31,7 +31,7 @@ async function generateAndAttachPDF(quotation) {
     <div style="width:100%;padding:0 32px;box-sizing:border-box;">
       <div style="display:flex;align-items:flex-start;gap:10px;">
       <div style="flex: 0 0 80px; display: flex; align-items: flex-start; justify-content: center;">
-        <img src='${process.env.BASE_URL}/public/ekaur5sitara.png' style='width:40px;height:50px;margin:0;'/>
+        <img src='https://cms-be.yogendersingh.tech/public/ekaur5sitara.png' style='width:40px;height:50px;margin:0;'/>
       </div>
         <div>
           <div style='font-size:18px;font-weight:bold;color:#222;'>FIVE STAR TECHNOLOGIES</div>
